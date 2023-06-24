b116ae5cec9f8a83fe51f836c573a8f35a8eeb8c
jrodriguez <jrodriguez@saludtools.com>
2023-06-23 21:19:49 -0500

    fix: update eks-cluster for deprecadated

--- eks_setup_terraform/eks-cluster.tf
+++ eks_setup_terraform/eks-cluster.tf
@@ -1,8 +1,12 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.5.1"
+
   cluster_name    = local.cluster_name
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+  cluster_version = "1.24"
+
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id = module.vpc.vpc_id
 
   tags = {
     Environment = "training"
@@ -10,28 +14,17 @@ module "eks" {
     GithubOrg   = "terraform-aws-modules"
   }
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
+  eks_managed_node_group_default = {
     root_volume_type = "gp2"
   }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    # {
-    #   name                          = "worker-group-2"
-    #   instance_type                 = "t2.medium"
-    #   additional_userdata           = "echo foo bar"
-    #   additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-    #   asg_desired_capacity          = 1
-    # },
-  ]
+  eks_managed_node_groups = {
+    one = {
+      name                          = "node-group-1"
+      instance_types                = "t2.small"
+      desired_size                  = 2
+    }
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
